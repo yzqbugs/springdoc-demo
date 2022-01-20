5e52e7a5cdee16606d4246ebf44cae0f8c527166
yzqdev <yzqdev@outlook.com>
2022-01-21 03:24:23 +0800

    add annotation

--- src/main/java/com/example/springdocdemo/controller/HelloController.java
+++ src/main/java/com/example/springdocdemo/controller/HelloController.java
@@ -15,7 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class HelloController {
     /**
-     *
+     * should be
+     * <pre>
+     *        "parameters": [
+     *           {
+     *             "name": "username",
+     *             "in": "query",
+     *             "description": "The user name for login",
+     *             "required": true,
+     *             "schema": {
+     *               "type": "string"
+     *             }
+     *           },
+     *           {
+     *             "name": "password",
+     *             "in": "query",
+     *             "description": "The password for login in clear text",
+     *             "required": true,
+     *             "schema": {
+     *               "type": "string"
+     *             }
+     *           }
+     *         ],
+     * </pre>
      *
      * @param user
      * @return {@link User}
@@ -24,6 +46,25 @@ public class HelloController {
     public User query(User user) {
         return user;
     }
+
+    /**
+     *should be
+     * <pre>
+     *      "requestBody": {
+     *                     "content": {
+     *                         "application/json": {
+     *                             "schema": {
+     *                                 "$ref": "#/components/schemas/User"
+     *                             }
+     *                         }
+     *                     },
+     *                     "required": true
+     *                 },
+     * </pre>
+     *
+     * @param user
+     * @return {@link User}
+     */
     @PostMapping("/body")
     public  User body(@RequestBody  User user) {
         return user;
